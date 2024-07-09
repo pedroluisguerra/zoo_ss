6a1dd6740faf3014f9c298fcc5d585d9a2a7b45d
Parce <pedroluisguerra@gmail.com>
2024-07-09 21:53:18 +0200

    class 09/07

--- app/modelos.py
+++ app/modelos.py
@@ -9,7 +9,11 @@ class TipoEntrada(Enum):
 
 class Entrada:
     def __init__(self, edad: int):
-        if edad <= 2:
+
+        if edad < 0:
+            raise ValueError("Edad no puede ser menor a 0")
+        
+        elif edad <= 2:
             self.tipo = TipoEntrada.BEBE
             self.precio = 0
         elif edad < 13:
@@ -21,3 +25,15 @@ class Entrada:
         else:
             self.tipo = TipoEntrada.JUBILADO
             self.precio = 18
+
+class GrupoEntrada:
+    def __init__(self):
+        self.total = 0
+        self.num_entradas = 0
+        
+
+    def agregar_entrada(self, edad: int):
+                
+        entrada = Entrada(edad)
+        self.total += entrada.precio
+        self.num_entradas += 1
--- tests/test_modelos.py
+++ tests/test_modelos.py
@@ -1,4 +1,5 @@
-from app.modelos import Entrada, TipoEntrada
+from app.modelos import Entrada, TipoEntrada, GrupoEntrada
+import pytest
 def test_crear_entrada():
     entrada = Entrada(12)
     assert entrada.tipo == TipoEntrada.NIÑO
@@ -15,3 +16,31 @@ def test_crear_entrada():
     entrada = Entrada(1)
     assert entrada.tipo == TipoEntrada.BEBE
     assert entrada.precio == 0
+
+def test_crear_entrada_edad_negativa_error():
+    with pytest.raises(ValueError):
+        Entrada(-2)
+        
+
+def test_crear_grupo_entradas():
+    grupo = GrupoEntrada()
+    assert grupo.total == 0
+    assert grupo.num_entradas == 0
+
+def test_add_entradas_grupo():
+    grupo = GrupoEntrada()
+    grupo.agregar_entrada(35)
+    assert grupo.num_entradas == 1
+    assert grupo.total == 23
+    
+    grupo.agregar_entrada(12)
+    assert grupo.num_entradas == 2
+    assert grupo.total == 37
+
+    grupo.agregar_entrada(72)
+    assert grupo.num_entradas == 3
+    assert grupo.total == 55
+
+    grupo.agregar_entrada(2)
+    assert grupo.num_entradas == 4
+    assert grupo.total == 55
